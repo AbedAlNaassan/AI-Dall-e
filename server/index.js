01ea95de47fb54b6faa51f18601ab92383707127
Abed El Rahman Al Naassan <abed.naassan@outlook.com>
2024-12-20 17:30:49 +0200

    index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes.js";
-import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "../routes/postRoutes.js";
+import dalleRoutes from "../routes/dalleRoutes.js";
 
 dotenv.config();
 
@@ -12,20 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello from Dall-E");
 });
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-const startServer = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(3000, () => console.log("Server has started on port 3000"));
-  } catch (error) {
-    console.log(error);
-  }
+// Vercel serverless function handler
+const handler = (req, res) => {
+  app(req, res);
 };
 
-startServer();
+export default handler;
