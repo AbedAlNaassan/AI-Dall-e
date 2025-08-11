c9f4eb97c6569840536d94318fdff3caba0de39a
Abed El Rahman Al Naassan <abed.naassan@outlook.com>
2025-08-11 16:26:14 +0300

    enhanced codes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["file-saver"]
-  }
+    include: ["file-saver"],
+  },
 });
